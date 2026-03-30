5bb5a64d421636fb373124c28e2861b4fcb9a980
Shawn Yang <yangshawn14@gmail.com>
2026-03-29 23:51:21 -0700

    Set up server.js

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+// import express
+const express = require('express');
+const quoteRoutes = require('./routes/quotes');
+
+// Create server
+const app = express();
+
+// Middleware
+app.use(express.json())
+
+// routes
+// Test route
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.use('/quotes', quoteRoutes);
+
+const PORT = 4010;
+// Start server
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log('Error setting up server.')
+    } else {
+        console.log(`Server is running on http://localhost:${PORT}`)
+    };
+
+})
