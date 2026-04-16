0b47b1bf5d7d35da9b35809c639fbe0fc32e763c
Shawn Yang <yangshawn14@gmail.com>
2026-04-15 19:06:51 -0700

    Add /quotes endpoint logic

--- backend/routes/quotes.js
+++ backend/routes/quotes.js
@@ -9,7 +9,15 @@ const carriers = require('../data/carriers.json');
 // POST quote details
 router.post('/', (req, res) => {
     // Get user input from req body
+    // console.log(req.body);
     const quoteRequest = req.body;
+
+    // Get age from quoteRequest.birthdate
+    const birthdate = new Date(quoteRequest.birthdate);
+    const currentDate = new Date();
+    const age = currentDate.getFullYear() - birthdate.getFullYear();
+
+
     // Generate a quote from each carrier
     const quotes = carriers.map((carrier) => {
         // create new carrier object for each carrier
@@ -19,6 +27,12 @@ router.post('/', (req, res) => {
             carrierLogo: carrier.carrierLogo,
             // Hard-coded premium for simplicity
             monthlyPremium: Math.floor(Math.random() * (100 - 25) + 25)
+            // If I wanted the premium to calculate more realistically instead of randomly
+            // monthlyPremium: Math.floor(
+            //     (quoteRequest.coverageAmount / 1000) *
+            //     (age / 50) *
+            //     (quoteRequest.tobaccoUse ? 1.5 : 1)
+            // )
         }
     });
     // send response to server
